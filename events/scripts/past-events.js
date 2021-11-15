2839f729b0481b7231e9c7e117e0c8a03bfecb10
void-ness <78547746+void-ness@users.noreply.github.com>
2021-11-15 23:48:59 +0530

    fixed: sep events

--- events/scripts/past-events.js
+++ events/scripts/past-events.js
@@ -1,10 +1,10 @@
 function slider(){
 
   // initialising slider
-
   let slides = document.getElementsByClassName("past-card");
   let slideIndex = 1;
   showSlides(slideIndex);
+
 
   // function to change slides number getting displayed
 
@@ -22,22 +22,28 @@ function slider(){
     }
     showSlides(slideIndex);
   }
-
+  
   //function to display required slides
 
   function showSlides(pos) {
+    // media query to show required no. of slides
+    let screenSize = window.matchMedia("(max-width: 450px)");
     let i;
     for (i = 0; i < slides.length; i++) {
-
         // makes all the slides disappear
         slides[i].style.display = "none";
         
-        // makes the required slides reappear
-        // slides[pos-1].style.display = "block";
-        slides[pos].style.display = "block";
-        // slides[pos+1].style.display = "block";
+    }
 
-    }  
+    // makes the required slides reappear
+    slides[pos-1].style.display = "block";
+    slides[pos].style.display = "block";
+
+    // shows 3 slides only when it is not the phone webpage
+    if (!screenSize.matches){
+      slides[pos+1].style.display = "block"; 
+    }
+
   }
 
   // adding click action to both the anchor tags
