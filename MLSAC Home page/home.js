38928df61fa05723da873864e4eaf7b2cf30a353
void-ness <78547746+void-ness@users.noreply.github.com>
2021-11-16 14:31:46 +0530

    fixed home page

--- MLSAC Home page/home.js
+++ MLSAC Home page/home.js
@@ -1,20 +1,13 @@
 let Loadvideo = document.getElementById("loadVideo");
 
 if(window.matchMedia("(max-width:450px)").matches){
-    Loadvideo.src="MLSAC Home page/images/phonebg.mp4"
+    Loadvideo.src="MLSAC Home page/images/phonebg.mp4";
+    
+    Loadvideo.addEventListener("ended", function(){
+        window.location="../aboutus.html"
+    })
 }
 
 setTimeout(function(){
     Loadvideo.style.animation="blurme 4s 1"
 }, 7900);
-
-Loadvideo.addEventListener("ended", function(){
-    // video disappears
-    // Loadvideo.style.opacity = "0";
-
-    // bg of video disappears
-    // document.getElementsByClassName("loading")[0].style.display = "none";
-    // headings gets blurred
-    document.getElementsByClassName("name")[0].style.animation = "blurme 3s 1";
-    document.getElementsByClassName("logo")[0].style.animation = "blurme 3s 1";
-})
